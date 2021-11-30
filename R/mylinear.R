9591dd25a79437cd38749218601f323a9db48ae7
cyclopenta <weizhou_qian@163.com>
2021-11-30 11:40:34 +0800

    first try

---
+++ R/mylinear.R
@@ -0,0 +1,224 @@
+#'mylm
+#'
+#'fit a linear model with basic model diagnose
+#'
+#'@param dat input data frame with no NAs, should include all the data that will be used
+#'@param response name string for response
+#'@param covariates string vector of the chosen variables in the input data frame
+#'@param category specify the categorical variables by input the names vector
+#'@param cat_method choose the coding method for categorical variables, should be 'refernce' or 'cellmeans'
+#'@param ref specify the reference level for each categorical variable
+#'@param model.diag default  is True, activate the model diagnose function
+#'@param intercept default is True, make the model with an intercept
+#'@param cutoff specify the level of significance for the test
+#'
+#'
+#'@return similar output as summary(lm), residuals,
+#'
+#'@examples
+#'mydata = read.sas7bdat("completedata.sas7bdat")
+#'t1 = mylm(mydata, 'Depression', covar1)
+#'t2 = mylm(mydata, 'Depression', covar1, intercept = F)
+#'t3 = mylm(mydata, 'Depression', covar2)
+#'t4 = mylm(mydata, 'Depression', covar2, category = c('Age_4Cat', 'NIHSS_4Cat'), ref = c(1,1))
+#'t5 = mylm(mydata, 'Depression', covar3, category = c('Age_4Cat'), cat_method = 'cellmeans', intercept = F)
+#'
+#'@export
+#'
+
+# dat should be a dataframe
+# response is the reponse variable
+# covariates are the corvarites
+# Test part
+
+setwd('D:/zl/Umich/course/biostat625/hw4test')
+library(sas7bdat)
+# included package
+library(ggplot2)
+library(dplyr)
+mydata = read.sas7bdat("completedata.sas7bdat")
+mylm = function(dat, response, covariates,
+                category = c(), cat_method = 'reference', ref = c(),
+                model.diag = T, intercept = T, cutoff = 0.05){
+  model_dat = subset(dat, select = c(response, covariates))
+  # checking the data
+  if (any(is.na(model_dat))){
+    stop("Nas exist among the selected variables")
+  }
+  # describe the data
+  obs = nrow(model_dat)
+
+  if(obs <= 1){
+    stop('do not input empty data or Need more observations ')
+  }
+  tmp_dat = subset(model_dat, select = covariates)
+  # categorize variables
+  if(length(category) >= 1 ){
+    model_dat = categorize(data = tmp_dat, vars = category, method = cat_method, ref = ref)
+    covariates = colnames(model_dat)
+    tmp_dat = model_dat
+  }
+  beta_n = length(covariates)
+  X_matrix = as.matrix(tmp_dat)
+  if (intercept){
+    X_matrix = cbind(rep(1, nrow(model_dat)), X_matrix)
+    beta_n = beta_n + 1
+    colnames(X_matrix) = c('intercept',covariates)
+  }
+  Y_matrix = as.matrix(subset(dat, select = response))
+  X_T = t(X_matrix)
+  # store the value of  the (t(X)X)-1 to save time
+  useful_matrix = solve((X_T %*% X_matrix))
+  # calculate betas
+  betas = useful_matrix %*% X_T %*% Y_matrix
+  # fitting the model
+  Hat = X_matrix %*% useful_matrix %*% X_T
+  Y_fitted = Hat %*% Y_matrix
+  res = Y_matrix - Y_fitted
+  res_T = t(res)
+  # estimating variance
+  MSE = ((res_T %*% res)/(obs - beta_n))[1,1]
+  betas_var = MSE * useful_matrix
+  betas_sd = sqrt(diag(betas_var))
+  # t-statistic
+  t_stats = c(betas/betas_sd)
+  p_val =  c(2 * (1 - pt(q = abs(t_stats), df = obs-beta_n)))
+  ifsign = ifelse(p_val <= cutoff, ifelse(p_val <= 0.01, ifelse(p_val<=0.001, '***', '**'),'*'), '-')
+  results = data.frame(coef = c(betas), std. = betas_sd, t.val = t_stats,
+                       p.val = p_val, significant = ifsign)
+  out_list =  list(results = results, residuals = res)
+  if (model.diag == T){
+    # Linearity dignose
+    # consider the SLR scenario
+    if(beta_n <= 2){
+      AXIS_Y = res
+      if (intercept){
+        AXIS_X = X_matrix[,2]
+      }
+      else{
+        AXIS_X = X_matrix[,1]
+      }
+      xlab = 'X'
+      ylab = 'residuals'
+      title = 'Residuals Plot'
+      df_pic = data.frame(x = AXIS_X, y = AXIS_Y)
+      colnames(df_pic) = c('x', 'y')
+      tmp_p = ggplot(df_pic, aes(x = x, y = y)) +
+        geom_point(shape = 5) + geom_smooth(method = lm)+
+        labs(x=xlab,y=ylab,title=title)+
+        theme_bw()+
+        theme(
+          plot.title = element_text(size = 18,face = "bold", vjust = 0.5, hjust = 0.5),
+          axis.title.x = element_text(size = 10,face = "bold"),
+          axis.title.y = element_text(size = 10,face = "bold"),
+          axis.text.x=element_text(size = 10,face = "bold"),
+          axis.text.y=element_text(size = 10,face = "bold"),
+        )
+      print(tmp_p)
+
+    }
+    else{
+      model_dignose(Y_matrix, covariates, tmp_dat)
+    }
+    # equal variance test
+    df_pic = data.frame(x = Y_fitted, y = res)
+    colnames(df_pic) = c('x', 'y')
+    xlab = 'Fitted values'
+    ylab = 'residuals'
+    title = "Plot for Constant Variance Test"
+    tmp_p2 = ggplot(df_pic, aes(x = x, y = y)) +
+      geom_point(shape = 5) +
+      labs(x=xlab,y=ylab,title=title)+
+      theme_bw()+
+      theme(
+        plot.title = element_text(size = 18,face = "bold", vjust = 0.5, hjust = 0.5),
+        axis.title.x = element_text(size = 10,face = "bold"),
+        axis.title.y = element_text(size = 10,face = "bold"),
+        axis.text.x=element_text(size = 10,face = "bold"),
+        axis.text.y=element_text(size = 10,face = "bold"),
+      )
+    print(tmp_p2)
+  }
+  return(out_list)
+}
+
+# process category variables
+categorize = function(data, vars, method = c('reference', 'cellmeans'), ref = c()){
+  method = match.arg(method)
+  l = nrow(data)
+  CAT_dat = select(data, -c(vars))
+  if (method == 'reference'){
+    if( length(ref) < 1){
+      stop('specified the reference')
+    }
+    i = 1
+    while (i <= length(vars)){
+      cats = unique(data[[vars[i]]])
+      cats = cats[cats != ref[i]]
+      w = length(cats)
+      # initialize the reference coding matrix
+      tmp = matrix(rep(data[[vars[i]]], w), nrow =l , ncol = w)
+      colnames(tmp) = paste(vars[i], cats,sep = '.')
+      j = 1
+      while (j <= length(cats)){
+        tmp[,j] = ifelse(tmp[,j] == cats[j], 1, 0)
+        j = j + 1
+      }
+      CAT_dat = cbind(CAT_dat, tmp)
+      i = i + 1
+    }
+  }
+  # cell means coding
+  else{
+    i = 1
+    while (i <= length(vars)){
+      cats = unique(data[[vars[i]]])
+      w = length(cats)
+      # initialize the reference coding matrix
+      tmp = matrix(rep(data[[vars[i]]], w), nrow =l , ncol = w)
+      colnames(tmp) = paste(vars[i], cats,sep = '.')
+      j = 1
+      while (j <= length(cats)){
+        tmp[,j] = ifelse(tmp[,j] == cats[j], 1, 0)
+        j = j + 1
+      }
+      CAT_dat = cbind(CAT_dat, tmp)
+      i = i + 1
+    }
+  }
+  return(CAT_dat)
+}
+
+# model diagnose using partial plots
+model_dignose = function(Y, covariates, modeldat){
+  n = length(covariates)
+  i = 1
+  ylab = paste(colnames(Y), 'others', sep = '|')
+  title = 'Partial Regression Plot'
+  while (i <= n){
+    tmp_X = select(modeldat, -c(covariates[i]))
+    new_vars  = colnames(tmp_X)
+    newdata = cbind(Y, tmp_X)
+    AXIS_Y = mylm(newdata, colnames(Y), new_vars, model.diag = F)[['residuals']]
+    choosen = select(modeldat, c(covariates[i]))
+    newdata = cbind(choosen, tmp_X)
+    AXIS_X = mylm(newdata, colnames(choosen), new_vars, model.diag = F)[['residuals']]
+    df_pic = data.frame(x = AXIS_X, y = AXIS_Y)
+    colnames(df_pic) = c('x', 'y')
+    xlab = paste(covariates[i], 'others', sep = '|')
+    tmp_p = ggplot(df_pic, aes(x = x, y = y)) +
+      geom_point(shape = 5) + geom_smooth(method = lm)+
+      labs(x=xlab,y=ylab,title=title)+
+      theme_bw()+
+      theme(
+        plot.title = element_text(size = 18,face = "bold", vjust = 0.5, hjust = 0.5),
+        axis.title.x = element_text(size = 10,face = "bold"),
+        axis.title.y = element_text(size = 10,face = "bold"),
+        axis.text.x=element_text(size = 10,face = "bold"),
+        axis.text.y=element_text(size = 10,face = "bold"),
+      )
+    print(tmp_p)
+    i = i + 1
+  }
+}
+
